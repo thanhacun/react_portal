54ddc741d921f618ddcf70e99edf26d4ec93ca73
THCN <thanhacun@gmail.com>
2017-11-08 10:50:35 +0700

    [UPD] remove added to gitignore out of repository

--- src/config/auth.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  appId: {
-    facebook: "1565459906908844",
-    //google: "canvas-hook-178304"
-    google: "520845368787-2agef4r0ig02fj9jnmogfuoc315242ep"
-  }
-};
-
-export default config;
